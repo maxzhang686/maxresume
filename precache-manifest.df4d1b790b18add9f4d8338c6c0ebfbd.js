9f7d9963ab15448b83e6fbe485d24ce83813a2bc
IVYMAX <ivyli@IVYMAXmbp.lan>
2019-08-20 04:07:06 +1000

    Updates

---
+++ precache-manifest.df4d1b790b18add9f4d8338c6c0ebfbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecaebca5bf11c0bcbd9af97178a7bf28",
+    "url": "/maxresume/index.html"
+  },
+  {
+    "revision": "118b6031034c31a114c5",
+    "url": "/maxresume/static/css/main.83ba1aa2.chunk.css"
+  },
+  {
+    "revision": "49f3db180476d8e06c83",
+    "url": "/maxresume/static/js/2.e3b9dd31.chunk.js"
+  },
+  {
+    "revision": "118b6031034c31a114c5",
+    "url": "/maxresume/static/js/main.3597811a.chunk.js"
+  },
+  {
+    "revision": "d2970fd81ce048a6d67e",
+    "url": "/maxresume/static/js/runtime~main.1a1732fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maxresume/static/media/logo.5d5d9eef.svg"
+  }
+]);
